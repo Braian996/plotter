9f8d717a4b4d21e4ca2eb93ccbb2f29971e8b4ba
alejandro99 <braian.ledesma-99@hotmail.com>
2017-12-03 00:08:37 -0300

    Agregado de mejora de secuencia

--- Plotter_V2.py
+++ Plotter_V2.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import sys
 sys.path.append('C:\\Python27\\lib\\site-packages\\wx-3.0-msw')
 import wx
@@ -6,14 +7,16 @@ import time
 
 #Variables
 
-#PROCESO PRINCIPAL
+#PROCESO PRINCIPAL GRAFICO
 class Main(wx.Frame):
-	rutaArchivo = ""
+
+	#Se inicializa el grafico
 	def __init__(self, parent, title):
 		wx.Frame.__init__(self, parent, title=title)
 		self.Centre()
 		self.InitUI()
 
+	#Se le agrega elementos al entorno
 	def InitUI(self):
 		#Variable de entorno
 		self.panel = wx.Panel(self)
@@ -87,33 +90,34 @@ class Main(wx.Frame):
 		#---FIN---
 		self.Show()
 
+	#Cierra el grafico
 	def OnQuit(self, event):
 		self.Close()
 	#Boton CONECTAR a la maquina
 	def OnConect(self, event):
 		global ser
 		puerto = ''
-		estado = event.GetEventObject().GetValue()
-		
-		if estado == True:
+		estado = event.GetEventObject().GetValue() #Obtiene el estado del boton al hacer click
+
+		if estado == True: #Si el estado da TRUE, inicia la conexion
 			#Busca el puerto disponible
 			for i in range(10):
 				try:
 					ser = serial.Serial(i)
-					puerto = ser.portstr
+					puerto = ser.portstr #Se guarda el puerto disponible
 					ser.close()
 				except:
 				#print('Nada')
 					pass
-			#---FIN---		
-			#CONEXION	
-			try:  
-				ser = serial.Serial()
+			#---FIN---
+			#CONEXION
+			try:
+				ser = serial.Serial() #Se asigna el objeto SERIAL
 				ser.port = puerto
-				ser.open()
-				print(ser.isOpen())
-				print('Conectado')
-				event.GetEventObject().SetLabel("Desconectar")
+				ser.open() #Abre la conexion
+				print(ser.isOpen()) #DEBUG
+				print('Conectado') #DEBUG
+				event.GetEventObject().SetLabel("Desconectar") #Se cambia el LABEL de boton
 				#Habilito los botones de control Plotter
 				self.btn_left5.Enable()
 				self.btn_left10.Enable()
@@ -124,14 +128,15 @@ class Main(wx.Frame):
 				self.btn_right15.Enable()
 				self.btn_right20.Enable()
 				self.btn_mover.Enable()
-				self.btn_open.Enable()
+				#self.btn_open.Enable()
 				#----FIN----
 			except: #Error de conexion
 				print("Error")
 				pass
-			#---FIN---		
-		else:
+			#---FIN---
+		else: #Si estado da FALSE, desconecta y cierra puerto SERIAL
 			ser.close()
+			#DESHABILITO LOS BOTONES
 			self.btn_left5.Disable()
 			self.btn_left10.Disable()
 			self.btn_left15.Disable()
@@ -141,11 +146,12 @@ class Main(wx.Frame):
 			self.btn_right15.Disable()
 			self.btn_right20.Disable()
 			self.btn_mover.Disable()
-			self.btn_open.Disable()
-			print(ser.isOpen())
-			event.GetEventObject().SetLabel("Conectar")
+			#self.btn_open.Disable()
+			#----FIN
+			print(ser.isOpen()) #DEBUG
+			event.GetEventObject().SetLabel("Conectar") #Se cambia el LABEL
 
-	#Control de Motor
+	#Control de Motor Correa
 	#Izquierda
 	def OnLeft5(self, event):
 		ser.write('r')
@@ -170,70 +176,92 @@ class Main(wx.Frame):
 		ser.write('u')
 	def OnRight20(self, event):
 		ser.write('i')
+	#---FIN MOTOR CORREA
 
+	#BOTON PARA ABRIR ARCHIVO GCODE (EN PRUEBA)
 	def OnOpen(self, event):
-		ventana = wx.Frame(None, -1, 'win.gcode')
-		ventana.SetDimensions(0, 0, 200, 50)
+		global rutaArchivo #Se declara de forma global la variable rutaArchivo
+		ventana = wx.Frame(None, -1, 'win.gcode') #Se crea la ventana
+		ventana.SetDimensions(0, 0, 200, 50) #Se da las dimensiones
 		#Crea el FileOpen Dialog
 		openFileDialog = wx.FileDialog(ventana, "Abrir archivo", "", "", "Archivos Gcodes (*.gcode)|*.gcode", wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
 		openFileDialog.ShowModal()
-		rutaArchivo = openFileDialog.GetPath()
-		print rutaArchivo
-		openFileDialog.Destroy()
+		rutaArchivo = openFileDialog.GetPath() #Asigna el path del archivo a la variable
+		print rutaArchivo #DEBUG
+		openFileDialog.Destroy() #Se destruye el objeto openFileDialog
 		pass
 
-		
+	#Dibuja el archivo GCODE
 	def OnMove(self, event):
-		archivo = open("circulo_20.gcode", "r")
-		#VERIFICACION
-		verXY = 0
+		#Se abre el archivo en forma de lectura y se lo asigna en una variable
+		archivo = open(rutaArchivo, "r")
+
 		#CONSTANTES
-		pasosMX = 0.41667
-		pasosMY = 0.11764
+		pasosMX = 0.41667 #Equivalente de 1 paso en motor X
+		pasosMY = 0.11764 #Equivalente de 1 paso en motor Y
 
 		#Variable de PASOS y DIRECCION motor
 		pm1 = 0
 		pm2 = 0
+		DirMX = 0
+		DirMY = 0
 
+		#Variable de INTERSECCION
 		intX = 0
 		intY = 0
 
-		pm1_dir = 0
-		pm2_dir = 0
 
 
-		#---FIN
-		cuchilla = 0
-		cuchilla_1 = 0
+
+		cuchilla = 0 #Variables de SIMULACION
+		cuchilla_1 = 0 #Variables de SIMULACION
+
+		#Variables de cant de Movimientos de GCODE
 		movX0 = 0
 		movY0 = 0
 		movX1 = 0
 		movY1 = 0
+
 		XD = 0
-		YD = 0 
+		YD = 0
+		#Variables auxiliares de motor
 		motorActualX = 0
 		motorActualY = 0
+
+		#Pasos de maquina
 		pasosx = 0
 		pasosy = 0
-		DirMX = 0
-		DirMY = 0
+
 
 		#Funcion que suma si hay coma en el array del numero
 		def convertir_num_decimal_string(numero):
-			tamano = len(numero)
-			cont = 0
+			tamano = len(numero) #Saca la cantidad de caracteres recibidos del String
+			cont = 0 #Inicializa el contador
 			while cont < tamano and numero[cont] != ".":	#Mientras no llegue al final, debe buscar la coma
 				cont += 1
-			
+
 			if cont == tamano:	#Veo, si son iguales, no hay coma
 				return numero
-			else:
+			else: #Si hay coma, elevo el numero
 				jojo = int(numero[0:cont])+1
 				return str(jojo)
 
-		#
+		#Se recorre el archivo
 		for renglon in archivo:
-			palabras = renglon.split(' ')
+
+			palabras = renglon.split(' ') #Se vectoriza todo el archivo en partes
+
+			#SI ENCUENTRA G02, G03 o G1 LOS CAMBIA POR G01
+			if palabras[0] == 'G02':
+				palabras[0] = palabras[0].replace("G02", "G01")
+			elif palabras[0] == 'G03':
+				palabras[0] = palabras[0].replace("G03", "G01")
+			else:
+				if palabras[0] == 'G1':
+					palabras[0] = palabras[0].replace("G1", "G01")
+
+
+			#COMPRUEBA PUNTOS INICIALES
 			if palabras[0] == 'G00':
 				if palabras[1][0] == 'Z' and palabras[1][1] == '-':
 					cuchilla = 1
@@ -243,23 +271,30 @@ class Main(wx.Frame):
 				if palabras[1][0] == 'X' and palabras[2][0] == 'Y':
 					movX0 = palabras[1].replace("X", "")
 					movY0 = palabras[2].replace("Y", "")
+					#Asigna los puntos iniciales en motorActual
 					motorActualX = float(movX0)
 					motorActualY = float(movY0)
-				
-			elif palabras[0] == 'G1':
+
+			elif palabras[0] == 'G01': #COMPRUEBA MOVIMIENTO
+				#Comprueba si hay X y Y
 				if palabras[1][0] == 'X' and palabras[2][0] == 'Y':
+					#Asigna el valor de X e Y en variables para sacar pasos y distancia
 					movX1 = palabras[1].replace("X", "")
 					movY1 = palabras[2].replace("Y", "")
 
-
+					#Comprueba el movimiento actual con el movimiento anterior para evitar repeticiones.
+					#Si no hay repeticiones
+					#Resta la posicion nueva con la que tiene asignado el motor actualmente
 					if float(movX1) != motorActualX:
 						XD = float(movX1) - motorActualX
 
-					if float(movY1) != motorActualY:	
+					if float(movY1) != motorActualY:
 						YD = float(movY1) - motorActualY
+					#FIN
 
 
-
+					#Al tener el valor de la cuenta anterior
+					#se verificara en que direccion se debe mover el motor
 					if XD > 0:
 						DirMX = 0
 						#pmX = 1
@@ -268,133 +303,134 @@ class Main(wx.Frame):
 						#pmX = 0
 					if YD > 0:
 						DirMY = 0
-					else: 
+					else:
 						DirMY = 1
 
-					pasosStrX = convertir_num_decimal_string(str(XD/pasosMX))		
-					pasosx = int(pasosStrX)
-					if pasosx < 0:
+					pasosStrX = convertir_num_decimal_string(str(XD/pasosMX)) #Se usa la funcion para corregir error de perdida de pasos
+					pasosx = int(pasosStrX) #Se convierte el valor a entero
+					if pasosx < 0: #Se convierte valores negativos de pasos a positivos
 						pasosx = pasosx * -1
-					pasosStrY = convertir_num_decimal_string(str(YD/pasosMY))	
+
+					pasosStrY = convertir_num_decimal_string(str(YD/pasosMY))
 					pasosy = int(pasosStrY)
 
 					if pasosy < 0:
 						pasosy *= -1
-					#print "PASOS X"+str(pasosx)+"PASOS Y"+str(pasosy)	
+					#print "PASOS X"+str(pasosx)+"PASOS Y"+str(pasosy)
+
 
 					motorActualX = float(movX1)
 					motorActualY = float(movY1)
-					
 
-					if pasosx > 1 and (pasosy >= 0 and pasosy <= 2):
-						verXY = 1
-						pm1 = 1
-						pm2 = 0
-						pm2_dir = 0
-						
-						for n in range(1,pasosx+1):
-							ser.write(chr(int('00000000',2)))
-							time.sleep(.002)
-							a = '0000'+str(pm1)+str(DirMX)+str(pm2)+str(DirMY)
-							d = int(str(a), 2)
-							ser.write(chr(d))
-							time.sleep(.002)
-						ser.write(chr(int('00000000',2)))
-							
-					else:
-					 	if (pasosx >= 0 and pasosx <= 1) and pasosy >= 2:
-					 		verXY = 0
-							pm1 = 0
-							pm2 = 1
-							
-							for j in range(1,pasosy+1):
-								ser.write(chr(int('00000000',2)))
-								time.sleep(.02)
-								a = '000000'+str(pm2)+str(DirMY)
-								b = int(str(a), 2)
-								ser.write(chr(b))
-								time.sleep(.02)
-							ser.write(chr(int('00000000',2)))
-								
-						else:
-							# EJE X y EJE Y IGUALES
-							if pasosx == pasosy:
-								print "hola"
+					#COMPROBACION PARA SECUENCIA DE MOVIMIENTOS
+					if pasosx > 0 or pasosy > 0:
+						contX = 0
+						contY = 0
+						#MOVIMIENTO DE AMBOS MOTORES
+						#X SUPERA A Y
+						if pasosx > pasosy and pasosy > 1:
+							while contX < (pasosx + 1):
+								intYaux = convertir_num_decimal_string(str(pasosy/pasosx)) #Calcula la interseccion de ambos motores
+								intY = int(intYaux)
 								pm1 = 1
-								pm2 = 1
-								for n in range (pasosx):
-									ser.write(chr(int('00000000',2)))
-									time.sleep(.05)
+								if contX == intY: # Si concuerda la interseccion asigna movimiento en motor Y
+									pm2 = 1
+									contX = 0 #Se resetea el contador de X
+								else:
+									pm2 = 0
+								ser.write(chr(int('00000000',2))) #Manda cero primero
+								time.sleep(.008)
+								a1 = '0000'+str(pm1)+str(DirMX)+str(pm2)+str(DirMY)
+								b1 = int(str(a1), 2)
+								ser.write(chr(b1)) #Manda movimiento
+								time.sleep(.008)
+								contX = contX + 1
+						else:
+							#Y SUPERA A X
+							if pasosy > pasosx and pasosx > 1:
+								while contY < (pasosy +1):
+									intXaux = convertir_num_decimal_string(str(pasosx/pasosy)) #Calcula la interseccion de ambos MOTORES
+									intX = int(intXaux)
+									pm2 = 1
+									if contY == intX: #Si concuerda la interseccion asigna movimiento en motor X
+										pm1 = 1
+										contY = 0
+									else:
+										pm1 = 0
+									ser.write(chr(int('00000000',2))) #Manda cero primero
+									time.sleep(.008)
 									a1 = '0000'+str(pm1)+str(DirMX)+str(pm2)+str(DirMY)
 									b1 = int(str(a1), 2)
-									ser.write(chr(b1))
-									time.sleep(.05)
-							else:		
-
-								#CIRCULO
-								if pasosx > 0 and pasosy > 0:
-									contY = 0
-									contX = 0
-									if pasosx > pasosy: #> 1
-										
-										while contX < (pasosx+1):
-											intYaux = convertir_num_decimal_string(str(pasosy/pasosx))	
-											intY = int(intYaux)
-											pm1 = 1
-											if contX == intY:
-												pm2 = 1
-												contX = 0
-												
-											else:
-												pm2 = 0	
-											ser.write(chr(int('00000000',2)))
-											time.sleep(.008)
-											a1 = '0000'+str(pm1)+str(DirMX)+str(pm2)+str(DirMY)
-											b1 = int(str(a1), 2)
-											ser.write(chr(b1))
-											time.sleep(.008)
-											contX = contX + 1
-											
-									elif pasosy > pasosx: #(pasosx / pasosy) > 1
-										
-										while contY < (pasosy+1): #(pasosy / pasosx)
-											intXaux = convertir_num_decimal_string(str(pasosx/pasosy))	
-											intX = int(intXaux)
+									ser.write(chr(b1)) #Manda movimiento
+									time.sleep(.008)
+									contX = contX + 1
+							else:
+								#MOVIMIENTO DE AMBOS MOTORES A LA PAR
+								if pasosx == pasosy and (pasosx > 1 and pasosy > 1):
+									pm1 = 1
+									pm2 = 1
+									for n in range (1, pasosx+1):
+										ser.write(chr(int('00000000',2))) #Manda cero primero
+										time.sleep(.05)
+										a1 = '0000'+str(pm1)+str(DirMX)+str(pm2)+str(DirMY)
+										b1 = int(str(a1), 2)
+										ser.write(chr(b1)) #Manda movimiento
+										time.sleep(.05)
+								else:
+									#MOVIMIENTO SOLO DE MOTOR X
+									if pasosx > 1 and (pasosy >= 0 and pasosy <= 1):
+										pm1 = 1
+										pm2 = 0
+										for n in range(1,pasosx+1):
+											ser.write(chr(int('00000000',2))) #Manda cero primero
+											time.sleep(.002)
+											a = '0000'+str(pm1)+str(DirMX)+str(pm2)+str(DirMY)
+											d = int(str(a), 2)
+											ser.write(chr(d)) #Manda movimiento
+											time.sleep(.002)
+										ser.write(chr(int('00000000',2)))
+									else:
+										#MOVIMIENTO SOLO DE MOTOR Y
+										if pasosy > 1 (pasosx >= 0 and pasosy <= 1):
+											pm1 = 0
 											pm2 = 1
-											if contY == intX:
-												pm1 = 1
-												contY = 0
-												
-											else:
-												pm1 = 0	
+											for n in range(1,pasosy+1):
+												ser.write(chr(int('00000000',2))) #Manda cero primero
+												time.sleep(.002)
+												a = '0000'+str(pm1)+str(DirMX)+str(pm2)+str(DirMY)
+												d = int(str(a), 2)
+												ser.write(chr(d)) #Manda movimiento
+												time.sleep(.002)
 											ser.write(chr(int('00000000',2)))
-											time.sleep(.008)
-											a1 = '0000'+str(pm1)+str(DirMX)+str(pm2)+str(DirMY)
-											b1 = int(str(a1), 2)
-											ser.write(chr(b1))
-											time.sleep(.008)
-											contY = contY + 1				
 
 
 
-											
-		archivo.close()							
 
-			
-			
+
+
+
+
+
+
+
+
+		archivo.close() #CIERRA EL ARCHIVO DE LECTURA
+
+
+
 
 
 
 
 #print cuchilla
 #print cuchilla_1
-	
 
 
-		
+
+
 
 	#---FIN---
 #FIN PROCESO PRINCIPAL
 app = wx.App()#Abre la APP
 Main(None, 'Plotter V2')#Se llama al proceso principal
-app.MainLoop()#Se crea el ciclo		
+app.MainLoop()#Se crea el ciclo
